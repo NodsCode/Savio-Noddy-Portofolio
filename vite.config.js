a732a6f589f73e48b15e5f858f302520b828dc4a
Your Name <saviooffice336@gmail.com>
2025-09-27 01:11:25 +0700

    fixed local and hosted server run

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // 👇 This is the key change for GitHub Pages
-  base: "/Savio-Noddy-Portofolio/",
-
+  base: process.env.NODE_ENV === "production" ? "/Savio-Noddy-Portofolio/" : "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
