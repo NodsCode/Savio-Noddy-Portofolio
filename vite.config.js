311e86c24fc2d8c84e0823f6f6b4e4e4a2ace681
Your Name <saviooffice336@gmail.com>
2025-09-27 01:30:18 +0700

    fixed some stuff

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/Savio-Noddy-Portofolio/" : "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
